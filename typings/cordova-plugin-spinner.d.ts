7544cdc51799fe06a9b96c9f0442d36c825ce74d
Justin Unterreiner <justin.unterreiner@11main.com>
2016-06-02 14:08:21 -0500

    Version bump to 1.1.0

--- typings/cordova-plugin-spinner.d.ts
+++ typings/cordova-plugin-spinner.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for cordova-plugin-spinner 1.0.0
+// Type definitions for cordova-plugin-spinner 1.1.0
 // Project: https://github.com/Justin-Credible/cordova-plugin-spinner
 // Definitions by: Justin Unterreiner <https://github.com/Justin-Credible>
 // Definitions: https://github.com/borisyankov/DefinitelyTyped
